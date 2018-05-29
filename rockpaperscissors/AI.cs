ae2f4be6041c44e018e246a22926241b930a8d4c
christopher saavedra <christosaavedra@gmail.com>
2018-05-29 17:50:19 -0500

    initialing commit

---
+++ rockpaperscissors/AI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockpaperscissors
+{
+    class AI : Player {
+
+    
+          public class Random = string("rock,paper,scissors,lizard,spock");{
+            }
+
+    }
+}
---
+++ rockpaperscissors/Human.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockpaperscissors
+{
+    class Human : Player
+    {
+        public override string MakeChoice()
+        {
+            Console.WriteLine("rock,paper,scissors,lizard,spock");
+            string input=Console.ReadLine();
+            switch (input)
+            {
+                case "rock":
+                    choice = input;
+                    break;
+                case "paper":
+                    choice = input;
+                    break;
+                case "scissor":
+                    choice = input;
+                    break;
+                case "lizard":
+                    choice = input;
+                    break;
+                case "spock":
+                    choice = input;
+                    break;
+                default:
+                    Console.WriteLine("please input correct response");
+                    return MakeChoice();
+
+            }
+            return input;
+        }
+    }
+}
